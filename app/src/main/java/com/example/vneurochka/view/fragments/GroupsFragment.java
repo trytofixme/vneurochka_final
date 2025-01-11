f50f674d50b3f964f7195d76eb3245d39c1dbcec
nsemenov <nsemenov@i-gs.ru>
2025-01-11 15:06:17 +0300

    Refactoring

--- app/src/main/java/com/example/vneurochka/view/fragments/GroupsFragment.java
+++ app/src/main/java/com/example/vneurochka/view/fragments/GroupsFragment.java
@@ -1,13 +1,15 @@
 package com.example.vneurochka.view.fragments;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -33,6 +35,7 @@ public class GroupsFragment extends Fragment {
     private GroupFragmentAdapter adapter;
     private DatabaseViewModel databaseViewModel;
     private List<Group> userGroupList = new ArrayList<>();
+    private Collection<Group> groupList = new ArrayList<>();
 
     public GroupsFragment()
     {
@@ -41,28 +44,36 @@ public class GroupsFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        initControls();
+        initUserGroups();
         View view = inflater.inflate(R.layout.fragment_groups, container, false);
+        setupRecycleView(view);
+        initSearchBar(view);
 
-        databaseViewModel = new ViewModelProvider(requireActivity()).get(DatabaseViewModel.class);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        linearLayoutManager.setReverseLayout(true);
-        linearLayoutManager.setStackFromEnd(true);
-
-        final RecyclerView recyclerView = view.findViewById(R.id.groups_list_recycle_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-        DividerItemDecoration mDividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), linearLayoutManager.getOrientation());
-        recyclerView.addItemDecoration(mDividerItemDecoration);
-
-        List<Group> groupList = databaseViewModel.getUsers();
-        adapter = new GroupFragmentAdapter(groupList);
-        recyclerView.setAdapter(adapter);
         return view;
     }
 
-    private void initControls() {
+    private void initSearchBar(View view) {
+        EditText et_search = view.findViewById(R.id.et_group_search);
+        et_search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                searchGroups(s.toString().toLowerCase());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (et_search.getText().toString().startsWith(" "))
+                    et_search.setText("");
+            }
+        });
+    }
+
+    private void initUserGroups() {
         String currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         DatabaseReference groupsDatabase = FirebaseDatabase.getInstance().getReference("Groups");
         //groupsDatabase.keepSynced(true);
@@ -76,7 +87,7 @@ public class GroupsFragment extends Fragment {
                         return;
                     }
 
-                    Collection<Group> groupList = groupHashMap.values();
+                    groupList = groupHashMap.values();
                     for (Group group: groupList) {
                         List<String> userList = group.getUserIds();
 
@@ -95,98 +106,32 @@ public class GroupsFragment extends Fragment {
                 }
             });
     }
-//    private void fetchingAllUserNAme() {
-//        databaseViewModel.fetchingUserDataCurrent();
-//        databaseViewModel.fetchUserCurrentData.observe(getViewLifecycleOwner(), new Observer<DataSnapshot>() {
-//            @Override
-//            public void onChanged(DataSnapshot dataSnapshot) {
-//                User users = dataSnapshot.getValue(User.class);
-//                assert users != null;
-//                currentUserId = users.getId();
-//            }
-//        });
-//
-//        databaseViewModel.fetchUserByNameAll();
-//        databaseViewModel.fetchUserNames.observe(getViewLifecycleOwner(), new Observer<DataSnapshot>() {
-//            @Override
-//            public void onChanged(DataSnapshot dataSnapshot) {
-//                if (et_search.getText().toString().equals("")) {
-//                    mUSer.clear();
-//                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-//                        User user = snapshot.getValue(User.class);
-//
-//                        assert user != null;
-//                        if (!(user.getEmail() == null)
-//                        ) {
-//                            if (!currentUserId.equals(user.getId())) {
-//                                mUSer.add(user);
-//
-//                            }
-//                        }
-//                        userFragmentAdapter = new UserFragmentAdapter(mUSer, context, false);
-//                        recyclerView.setAdapter(userFragmentAdapter);
-//
-//                    }
-//
-//                }
-//            }
-//        });
-//    }
-//
-//    private void init(View view) {
-//        databaseViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory
-//                .getInstance(requireActivity().getApplication()))
-//                .get(DatabaseViewModel.class);
-//
-//        recyclerView = view.findViewById(R.id.user_list_recycle_view);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(context));
-//        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
-//        recyclerView.addItemDecoration(dividerItemDecoration);
-//        mUSer = new ArrayList<>();
-//        et_search = view.findViewById(R.id.et_search);
-//        et_search.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                    searchGroups(s.toString().toLowerCase());
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//                if (et_search.getText().toString().startsWith(" "))
-//                    et_search.setText("");
-//            }
-//        });
-//
-//    }
-//
-//    private void searchGroups(String searchText) {
-//
-//        if (!(searchText.isEmpty() && searchText.equals(""))) {
-//            databaseViewModel.fetchSearchedUser(searchText);
-//            databaseViewModel.fetchSearchUser.observe(this, new Observer<DataSnapshot>() {
-//                @Override
-//                public void onChanged(DataSnapshot dataSnapshot) {
-//                    mUSer.clear();
-//                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-//                        User users = snapshot.getValue(User.class);
-//                        assert users != null;
-//                        if (!users.getId().equals(currentUserId)) {
-//                            mUSer.add(users);
-//                        }
-//
-//                    }
-//                    userFragmentAdapter = new UserFragmentAdapter(mUSer, context, false);
-//                    recyclerView.setAdapter(userFragmentAdapter);
-//
-//                }
-//            });
-//        }else {
-//            fetchingAllUserNAme();
-//        }
-//    }
+
+    private void setupRecycleView(View view) {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        final RecyclerView recyclerView = view.findViewById(R.id.groups_list_recycle_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        DividerItemDecoration mDividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), linearLayoutManager.getOrientation());
+        recyclerView.addItemDecoration(mDividerItemDecoration);
+
+        adapter = new GroupFragmentAdapter(userGroupList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void searchGroups(String searchText) {
+        ArrayList<Group> requestedGroups = new ArrayList<>();
+        for (Group group : groupList) {
+            if (group.getName().contains(searchText)) {
+                requestedGroups.add(group);
+            }
+        }
+
+        adapter.setItems(requestedGroups);
+        adapter.notifyDataSetChanged();
+    }
 }
